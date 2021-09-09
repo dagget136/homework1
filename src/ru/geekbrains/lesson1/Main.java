d7c0d7f0d3b98b0a68aa10518aa00d4b46994285
VladimirToryanik <degget95@gmail.com>
2021-09-09 14:58:50 +0300

    Upgrade HW1

--- src/ru/geekbrains/lesson1/Main.java
+++ src/ru/geekbrains/lesson1/Main.java
@@ -1,7 +1,5 @@
 package ru.geekbrains.lesson1;
-
 class HomeWorkApp {
-
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
@@ -17,32 +15,28 @@ class HomeWorkApp {
         int a = 5;
         int b = 3;
         if (a +b >= 0) {
-            System.out.println("сумма положительная");
+        System.out.println("сумма положительная");
         } else {
-                System.out.println("сумма отрицательная");
+        System.out.println("сумма отрицательная");
             }
     }
         static void printColor() {
             int value = 101;
             if (value <= 0) {
-                System.out.println("Красный");
+        System.out.println("Красный");
             } else if (value <= 100) {
-                System.out.println("Желтый");
+        System.out.println("Желтый");
             } else if (value > 100) {
-                System.out.println("Зеленый");
+        System.out.println("Зеленый");
             }
-
         }
         static void compareNumbers() {
         int c = 10;
         int d = 10;
         if (c >= d) {
-            System.out.println(c + ">=" + d);
+        System.out.println(c + ">=" + d);
         }else if (c < d) {
-            System.out.println(c + "<" + d);
+        System.out.println(c + "<" + d);
         }
         }
-
-
-
 }
